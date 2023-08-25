92ff3a08153d74bed397979a6abcdee74ebb3f9d
Gent (PG/R - Comp Sci & Elec Eng) <jiantao.wu@surrey.ac.uk>
2023-08-25 22:07:00 +0100

    vit+wandb

--- main.py
+++ main.py
@@ -5,6 +5,7 @@
 # LICENSE file in the root directory of this source tree.
 
 from pathlib import Path
+import wandb
 import argparse
 import json
 import math
@@ -28,9 +29,9 @@ parser.add_argument('--workers', default=8, type=int, metavar='N',
                     help='number of data loader workers')
 parser.add_argument('--epochs', default=1000, type=int, metavar='N',
                     help='number of total epochs to run')
-parser.add_argument('--batch-size', default=2048, type=int, metavar='N',
+parser.add_argument('--batch-size', default=64, type=int, metavar='N',
                     help='mini-batch size')
-parser.add_argument('--learning-rate-weights', default=0.2, type=float, metavar='LR',
+parser.add_argument('--learning-rate-weights', default=5e-4, type=float, metavar='LR',
                     help='base learning rate for weights')
 parser.add_argument('--learning-rate-biases', default=0.0048, type=float, metavar='LR',
                     help='base learning rate for biases and batch norm parameters')
@@ -42,7 +43,7 @@ parser.add_argument('--projector', default='8192-8192-8192', type=str,
                     metavar='MLP', help='projector MLP')
 parser.add_argument('--print-freq', default=100, type=int, metavar='N',
                     help='print frequency')
-parser.add_argument('--checkpoint-dir', default='./checkpoint/', type=Path,
+parser.add_argument('--output_dir', default='./outputs/', type=Path,
                     metavar='DIR', help='path to checkpoint directory')
 
 
@@ -77,8 +78,8 @@ def main_worker(gpu, args):
         world_size=args.world_size, rank=args.rank)
 
     if args.rank == 0:
-        args.checkpoint_dir.mkdir(parents=True, exist_ok=True)
-        stats_file = open(args.checkpoint_dir / 'stats.txt', 'a', buffering=1)
+        args.output_dir.mkdir(parents=True, exist_ok=True)
+        stats_file = open(args.output_dir / 'stats.txt', 'a', buffering=1)
         print(' '.join(sys.argv))
         print(' '.join(sys.argv), file=stats_file)
 
@@ -96,13 +97,11 @@ def main_worker(gpu, args):
             param_weights.append(param)
     parameters = [{'params': param_weights}, {'params': param_biases}]
     model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[gpu])
-    optimizer = LARS(parameters, lr=0, weight_decay=args.weight_decay,
-                     weight_decay_filter=True,
-                     lars_adaptation_filter=True)
+    optimizer = optim.AdamW(parameters, lr=1, weight_decay=args.weight_decay)
 
     # automatically resume from checkpoint if it exists
-    if (args.checkpoint_dir / 'checkpoint.pth').is_file():
-        ckpt = torch.load(args.checkpoint_dir / 'checkpoint.pth',
+    if (args.output_dir / 'checkpoint.pth').is_file():
+        ckpt = torch.load(args.output_dir / 'checkpoint.pth',
                           map_location='cpu')
         start_epoch = ckpt['epoch']
         model.load_state_dict(ckpt['model'])
@@ -112,12 +111,15 @@ def main_worker(gpu, args):
 
     dataset = torchvision.datasets.ImageFolder(args.data / 'train', Transform())
     sampler = torch.utils.data.distributed.DistributedSampler(dataset)
-    assert args.batch_size % args.world_size == 0
+    args.batch_size = args.batch_size * args.world_size
     per_device_batch_size = args.batch_size // args.world_size
     loader = torch.utils.data.DataLoader(
         dataset, batch_size=per_device_batch_size, num_workers=args.workers,
         pin_memory=True, sampler=sampler)
 
+    run = None
+    if args.rank == 0:
+        run = wandb.init(project="ssl",entity="dlib",name="barlowtwins",job_type="pretrain")
     start_time = time.time()
     scaler = torch.cuda.amp.GradScaler()
     for epoch in range(start_epoch, args.epochs):
@@ -136,20 +138,22 @@ def main_worker(gpu, args):
                 if args.rank == 0:
                     stats = dict(epoch=epoch, step=step,
                                  lr_weights=optimizer.param_groups[0]['lr'],
-                                 lr_biases=optimizer.param_groups[1]['lr'],
                                  loss=loss.item(),
                                  time=int(time.time() - start_time))
                     print(json.dumps(stats))
                     print(json.dumps(stats), file=stats_file)
-        if args.rank == 0:
+            if run == 0:
+                run.log({"epoch":epoch,"step":step,"lr":optimizer.param_groups[0]['lr'],"loss":loss.item()})
+        if args.rank == 0 and epoch % 20 == 0:
             # save checkpoint
+            
             state = dict(epoch=epoch + 1, model=model.state_dict(),
                          optimizer=optimizer.state_dict())
-            torch.save(state, args.checkpoint_dir / 'checkpoint.pth')
+            torch.save(state, args.output_dir / f'checkpoint_{epoch+1}.pth')
     if args.rank == 0:
         # save final model
         torch.save(model.module.backbone.state_dict(),
-                   args.checkpoint_dir / 'resnet50.pth')
+                   args.output_dir / 'checkpoint.pth')
 
 
 def adjust_learning_rate(args, optimizer, loader, step):
@@ -164,8 +168,8 @@ def adjust_learning_rate(args, optimizer, loader, step):
         q = 0.5 * (1 + math.cos(math.pi * step / max_steps))
         end_lr = base_lr * 0.001
         lr = base_lr * q + end_lr * (1 - q)
+    lr = max(lr, 1e-6)
     optimizer.param_groups[0]['lr'] = lr * args.learning_rate_weights
-    optimizer.param_groups[1]['lr'] = lr * args.learning_rate_biases
 
 
 def handle_sigusr1(signum, frame):
@@ -183,16 +187,16 @@ def off_diagonal(x):
     assert n == m
     return x.flatten()[:-1].view(n - 1, n + 1)[:, 1:].flatten()
 
-
+from vision_transformer import vit_base
+# from timm.models.vision_transformer import vit_base_patch16_224
 class BarlowTwins(nn.Module):
     def __init__(self, args):
         super().__init__()
         self.args = args
-        self.backbone = torchvision.models.resnet50(zero_init_residual=True)
-        self.backbone.fc = nn.Identity()
+        self.backbone = vit_base(num_classes=0)
 
         # projector
-        sizes = [2048] + list(map(int, args.projector.split('-')))
+        sizes = [self.backbone.embed_dim] + list(map(int, args.projector.split('-')))
         layers = []
         for i in range(len(sizes) - 2):
             layers.append(nn.Linear(sizes[i], sizes[i + 1], bias=False))
